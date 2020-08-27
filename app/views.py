fc834030d0356605f5aa738c9d9398720ec0fa14
Charitha Weerasooriya <charithaw1234@gmail.com>
2020-08-27 15:23:07 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,34 @@
+from app import app
+import base64
+from flask import request, render_template
+from IPython.utils import io
+from synthesizer.inference import Synthesizer
+from encoder import inference as encoder
+from vocoder import inference as vocoder
+from pathlib import Path
+import numpy as np
+import librosa
+from base64 import b64decode
+import uuid
+encoder_weights = Path("encoder/saved_models/pretrained.pt")
+vocoder_weights = Path("vocoder/saved_models/pretrained/pretrained.pt")
+syn_dir = Path("synthesizer/saved_models/logs-pretrained/taco_pretrained")
+encoder.load_model(encoder_weights)
+synthesizer = Synthesizer(syn_dir)
+vocoder.load_model(vocoder_weights)
+
+@app.route('/')
+def index():
+    text = "Tonight, I am asking you to believe in Joe and Kamala’s ability to lead this country"
+    in_fpath = Path("audio.wav")
+    reprocessed_wav = encoder.preprocess_wav(in_fpath)
+    original_wav, sampling_rate = librosa.load(in_fpath)
+    preprocessed_wav = encoder.preprocess_wav(original_wav, sampling_rate)
+    embed = encoder.embed_utterance(preprocessed_wav)
+    with io.capture_output() as captured:
+        specs = synthesizer.synthesize_spectrograms([text], [embed])
+    generated_wav = vocoder.infer_waveform(specs[0])
+    generated_wav = np.pad(generated_wav, (0, synthesizer.sample_rate), mode="constant")
+    with open('generated.wav','wb+') as f:
+        f.write(generated_wav)
+    return render_template("audio.html")
