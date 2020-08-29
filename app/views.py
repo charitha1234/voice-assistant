a1772ea749e3017ba2be0ffd01874b58d181f2f1
Charitha Weerasooriya <charithaw1234@gmail.com>
2020-08-29 17:39:59 +0530

    dep missmatch

--- app/views.py
+++ app/views.py
@@ -9,9 +9,9 @@ import numpy as np
 import librosa
 from base64 import b64decode
 import uuid
-encoder_weights = Path("encoder/saved_models/pretrained.pt")
-vocoder_weights = Path("vocoder/saved_models/pretrained/pretrained.pt")
-syn_dir = Path("synthesizer/saved_models/logs-pretrained/taco_pretrained")
+encoder_weights = Path("../encoder/saved_models/pretrained.pt")
+vocoder_weights = Path("../vocoder/saved_models/pretrained/pretrained.pt")
+syn_dir = Path("../synthesizer/saved_models/logs-pretrained/taco_pretrained")
 encoder.load_model(encoder_weights)
 synthesizer = Synthesizer(syn_dir)
 vocoder.load_model(vocoder_weights)
